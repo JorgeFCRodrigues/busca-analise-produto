3e4af956597512a1638e7736029ff8a4ae386caa
Jorge Rodrigues <jorgefc.rodrigues25@gmail.com>
2025-11-07 11:12:04 -0300

    🚀 Implementa script para busca e análise de produtos no Mercado Livre, exportando resultados para Excel.

---
+++ mercado_livre.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait 
+
+import pandas as pd
+import time
+
+entrada = input('Digite o produto que deseja pesquisar: ')
+
+driver = webdriver.Chrome()
+driver.get('https://www.mercadolivre.com.br/')
+wait = WebDriverWait(driver, 8)
+time.sleep(2)
+driver.find_element(By.CLASS_NAME, 'nav-search-input').send_keys(entrada+Keys.ENTER)
+time.sleep(3)
+
+items = driver.find_elements(By.XPATH, '//li[@class="ui-search-layout__item"]')
+print(f"{len(items)} produtos encontrados.")
+
+dados_ml = []
+
+for item_ml in items:
+    title = item_ml.find_element(By.TAG_NAME, 'h3').text
+    price = ""
+    link = item_ml.find_element(By.CLASS_NAME, 'poly-component__title').get_attribute('href')
+    avaliation = ""
+    qtd_sellers = ""
+
+    try:
+        price = item_ml.find_element(
+            By.CLASS_NAME, 
+            'andes-money-amount.andes-money-amount--cents-superscript'
+        ).text.replace('\n','')
+    except:
+        pass
+
+    try:
+        avaliation = item_ml.find_elements(
+            By.CLASS_NAME,
+            'poly-phrase-label'
+        )
+        for aval in avaliation:
+            if '|' in aval.text:
+                qtd_sellers = aval.text
+                qtd_sellers = qtd_sellers.replace('|','').strip()
+                qtd_sellers = qtd_sellers.replace(' vendidos','')
+                qtd_sellers = int(qtd_sellers.replace('mil','000'))
+            else:
+                avaliation = aval.text
+    except Exception as e:
+        print(e)
+        pass
+
+    dados_ml.append({
+        'title': title,
+        'price': price,
+        'link': link,
+        'avaliation': avaliation,
+        'qtd_sellers': qtd_sellers
+    })
+
+df_ml = pd.DataFrame(dados_ml)
+print(dados_ml)
+df_ml.to_excel(f"dados_Mercado_Livre.xlsx", index=False)
+print(f"\nArquivo Excel 'dados_Mercado_Livre.xlsx' criado com sucesso!")
+
+driver.quit()
+
+# driver.execute_script("window.open('https://www.kabum.com.br//');")
+# driver.switch_to.window(driver.window_handles[2])
+# time.sleep(5)
